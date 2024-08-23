46c5e9e815f5e66b51fc1723c0bf80def570cbf4
NoureenAyman <noureenayman7@gmail.com>
2024-08-23 04:34:44 +0300

    finished home screen. added bottom navigation bar.

--- lib/app_colors.dart
+++ lib/app_colors.dart
@@ -1,6 +1,8 @@
 import 'dart:ui';
 
 class AppColors{
+  static const Color blackColor= Color(0xff242424);
   static const  Color primarylightcolor= Color(0xffB7935F);
+  static const Color whiteColor= Color(0xfffffffF);
   
 }
